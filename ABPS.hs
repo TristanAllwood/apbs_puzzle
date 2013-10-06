0803fcf3d3ee95ffa5fbd258060266c51611031d
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2013-10-06 13:09:23 +0100

    List comps not monads.

--- ABPS.hs
+++ ABPS.hs
@@ -1,48 +1,34 @@
 module ABPS where
 
-import Control.Monad
+{- there are two numbers each between 2 and 99 inclusive. -}
+{- call them a and b -}
 
-solution = do
-  {- there are two numbers each between 2 and 99 inclusive. -}
-  {- call them a and b -}
-  a <- [2..99]
-  b <- [2..a]
+{- there are two participants, p and s. -}
+{- p only knows the product of a and b -}
+{- s only knows the sum of a and b -}
+p0 (a, b) = a * b
+s0 (a, b) = a + b
 
-  let p0 (a, b) = a * b
-  let s0 (a, b) = a + b
+{- p says "I don't know a and b" -}
+p1 = multiple_solutions . factors . p0
 
-  {- there are two participants, p and s. -}
-  {- p only knows the product of a and b -}
-  {- s only knows the sum of a and b -}
-  let p = p0 (a, b)
-  let s = s0 (a, b)
+{- s says "I know you don't know" -}
+s1 = all p1 . components . s0
 
-  {- p says "I don't know a and b" -}
-  let p1 = multiple_solutions . factors
+{- p now says "I know what the numbers are" -}
+p2 = single_solution . filter s1 . factors . p0
 
-  guard (p1 p)
-
-
-  {- s says "I know you don't know" -}
-  let s1 = all (p1 . p0) . components
-
-  guard (s1 s)
+{- s finally says "I know what the numbers are" -}
+s2 = single_solution . filter p2 . components . s0
 
 
-  {- p now says "I know what the numbers are" -}
-  let p2 = single_solution . filter (s1 . s0) . factors
-
-  guard (p2 p)
-
-
-  {- s finally says "I know what the numbers are" -}
-  let s2 = single_solution . filter (p2 . p0) . components
-
-  guard (s2 s)
-
-
-  {- thus, the numbers are... -}
-  return (a,b)
+solution = [ ab | a <- [2..99], b <- [2..a],
+                  let ab = (a,b),
+                  p1 ab,
+                  s1 ab,
+                  p2 ab,
+                  s2 ab
+           ]
 
 single_solution :: [a] -> Bool
 single_solution [_] = True
@@ -52,18 +38,14 @@ multiple_solutions :: [a] -> Bool
 multiple_solutions = not . null . drop 1
 
 factors :: Int -> [(Int, Int)]
-factors n = do
-  x <- [2..(floor (sqrt (fromIntegral n)))]
-
-  guard ((n `mod` x) == 0)
-  let y = n `div` x
-  guard (y >= 2 && y <= 99)
-
-  return (x, y)
+factors n = [ (x,y) | x <- [2..min 99 (floor . sqrt . fromIntegral $ n)],
+                      (n `mod` x) == 0,
+                      let y = n `div` x,
+                      y >= 2 && y <= 99
+            ]
 
 components :: Int -> [(Int, Int)]
-components n = do
-  x <- [2..(n `div` 2) + 1]
-  let y = n - x
-  guard (y >= x && y >= 2 && y <= 99)
-  return (x,y)
+components n = [ (x,y) | x <- [2..min 99 ((n `div` 2) + 1)],
+                         let y = n - x,
+                         y >= x && y >= 2 && y <= 99
+               ]
