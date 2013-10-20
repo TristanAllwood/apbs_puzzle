3fe6cc06a3b1626535043f2a9e1d0a1237b2fa44
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2013-10-20 20:21:25 +0100

    Updates for lhug.

--- ABPS.hs
+++ ABPS.hs
@@ -8,47 +8,47 @@ module ABPS where
 {- s only knows the sum of a and b -}
 
 
-p0 (a, b) = factors (a * b)
-s0 (a, b) = components (a + b)
-{- p0 and s0 give the possible (a,b)'s for p and s. -}
+p (a, b) = multiplicands (a * b)
+s (a, b) = summands (a + b)
+{- p and s give the possible (a,b)'s for p and s. -}
 
 {- p says "I don't know a and b" -}
-p1 = multiple_solutions . p0
+p1 = more_than_one . p
 
 {- s says "I know you don't know" -}
-s1 = all p1 . s0
+s1 = all p1 . s
 
 {- p now says "I know what the numbers are" -}
-p2 = single_solution . filter s1 . p0
+p2 = exactly_one . filter s1 . p
 
 {- s finally says "I know what the numbers are" -}
-s2 = single_solution . filter p2 . s0
+s2 = exactly_one . filter p2 . s
 
 
-solution = [ ab | a <- [2..99], b <- [2..a],
-                  let ab = (a,b),
-                  p1 ab,
-                  s1 ab,
-                  p2 ab,
-                  s2 ab
+solution = [ (a,b)
+           | a <- [2..99], b <- [2..a]
+           , p1 (a,b)
+           , s1 (a,b)
+           , p2 (a,b)
+           , s2 (a,b)
            ]
 
-single_solution :: [a] -> Bool
-single_solution [_] = True
-single_solution _   = False
+exactly_one :: [a] -> Bool
+exactly_one [_] = True
+exactly_one _   = False
 
-multiple_solutions :: [a] -> Bool
-multiple_solutions = not . null . drop 1
+more_than_one :: [a] -> Bool
+more_than_one = not . null . drop 1
 
-factors :: Int -> [(Int, Int)]
-factors n = [ (x,y) | x <- [2..min 99 (floor . sqrt . fromIntegral $ n)],
+multiplicands :: Int -> [(Int, Int)]
+multiplicands n = [ (x,y) | x <- [2..min 99 (floor . sqrt . fromIntegral $ n)],
                       (n `mod` x) == 0,
                       let y = n `div` x,
                       y >= 2 && y <= 99
             ]
 
-components :: Int -> [(Int, Int)]
-components n = [ (x,y) | x <- [2..min 99 ((n `div` 2) + 1)],
+summands :: Int -> [(Int, Int)]
+summands n = [ (x,y) | x <- [2..min 99 (n `div` 2)],
                          let y = n - x,
                          y >= x && y >= 2 && y <= 99
                ]
---
+++ Viz.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Viz where
+
+import ABPS
+import Data.Data
+
+import Data.Aeson.Generic
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as C
+
+data Table
+  = Table { titles :: [String]
+          , rows   :: [[Value]]
+          }
+  deriving (Data, Typeable)
+
+data Value = AandB (Int, Int)
+           | Value Int
+           | NestedTable Table
+           | Several [Value]
+           | Truth Bool
+  deriving (Data, Typeable)
+
+p_table abs
+  = Table { titles = ["(a,b) are",   "p sees a * b", "p thinks (a,b) could be"]
+          , rows   = [ [AandB (a,b), Value (a*b),    Several [AandB v | v <- p (a,b)] ]
+                     | (a,b) <- abs
+                     ]
+          }
+
+s_table abs
+  = Table { titles = ["(a,b) are",   "s sees a + b", "s thinks (a,b) could be"]
+          , rows   = [ [AandB (a,b), Value (a+b),    Several [AandB v | v <- s (a,b)] ]
+                     | (a,b) <- abs
+                     ]
+          }
+
+p1_table abs
+  = Table { titles = titles pt ++ ["p sees multiple possibilities?"]
+          , rows   = [ row ++ [Truth (p1 ab)] | row@(AandB ab:_) <- rows pt ]
+          }
+  where
+    pt = p_table abs
+
+s1_table abs
+  = Table { titles = ["(a,b) are", "s sees a + b",
+                      "s thinks what (a,b) could be and pretends to run p1",
+                      "s knows p can only see multiple possibilities?"]
+          , rows   = [ [ AandB (a,b), Value (a+b)
+                       , NestedTable $ p1_table (s (a,b))
+                       , Truth (s1 (a,b))
+                       ]
+                     | (a,b) <- abs
+                     ]
+          }
+
+p2_table abs
+  = Table { titles = [ "(a,b) are", "p sees a * b",
+                       "p thinks what (a,b) could be and pretends to run s1",
+                       "p knows s sees exactly one option?" ]
+          , rows   = [ [ AandB (a,b), Value (a*b)
+                       , NestedTable $ s1_table (p (a,b))
+                       , Truth (p2 (a,b))
+                       ]
+                     | (a,b) <- abs
+                     ]
+          }
+
+s2_table abs
+  = Table { titles = [ "(a,b) are", "s sees a + b",
+                       "s thinks what (a,b) could be and pretends to run p2",
+                       "s knows p sees exactly one option?"]
+          , rows   = [ [ AandB (a,b), Value (a+b)
+                       , NestedTable $ p2_table (s (a,b))
+                       , Truth (s2 (a,b))
+                       ]
+                     | (a,b) <- abs
+                     ]
+          }
+
+main = do
+
+  let all_abs = [ (a,b) | a <- [2..99], b <- [2..a] ]
+  let interesting_abs = [(2,2), (6,5), (13,4)]
+
+  L.writeFile "p.js" (C.pack "p_table = " `L.append` (encode $ p_table interesting_abs))
+  L.writeFile "s.js" (C.pack "s_table = " `L.append` (encode $ s_table interesting_abs))
+
+  L.writeFile "p1.js" (C.pack "p1_table = " `L.append` (encode $ p1_table interesting_abs))
+  L.writeFile "s1.js" (C.pack "s1_table = " `L.append` (encode $ s1_table interesting_abs))
+
+  L.writeFile "p2.js" (C.pack "p2_table = " `L.append` (encode $ p2_table interesting_abs))
+  L.writeFile "s2.js" (C.pack "s2_table = " `L.append` (encode $ s2_table interesting_abs))
